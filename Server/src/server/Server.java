58b3b80363b7377a304f372ea460c69b8c5f1237
Daniel Lovegrove <d.lovegrove11@gmail.com>
2017-11-29 19:30:54 -0600

    Clean up Server code, add thread pool to it

--- Server/src/server/Server.java
+++ Server/src/server/Server.java
@@ -4,7 +4,17 @@ import java.io.*;
 import java.net.*;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ExecutorService;
 
+
+/**
+ * Server that Clients can connect to via TCP/IP. This server can act as a
+ * normal server, where clients can connect to it, or as a proxy server to a
+ * single other server. The type of server (normal/proxy) that this is depends
+ * on which constructor is used to create an instance of it.
+ * @author loveboat
+ */
 public class Server implements Runnable {
 
     private ServerSocket serverSocket = null;
@@ -13,6 +23,7 @@ public class Server implements Runnable {
     public userinterface.UserInterface myUI;
     private int portNumber = 8765, backlog = 500;
     private boolean doListen = false;
+    private ExecutorService threadPool = null;
 
     private boolean isProxyServer = false;
     private InetAddress proxyAddress = null;
@@ -31,6 +42,7 @@ public class Server implements Runnable {
         this.backlog = backlog;
         this.myUI = myUI;
         this.myClientCommandHandler = new clientmessagehandler.ClientMessageHandler(this);
+        this.threadPool = Executors.newFixedThreadPool(16);
     }
 
 
@@ -51,12 +63,13 @@ public class Server implements Runnable {
         this.proxyPortNumber = proxyPortNumber;
         this.isProxyServer = true;
         this.myClientCommandHandler = new clientmessagehandler.ClientMessageHandler(this);
+        this.threadPool = Executors.newFixedThreadPool(16);
     }
 
 
-    public synchronized void setDoListen(boolean doListen){
-        this.doListen = doListen;
-    }
+    /**
+     * Create a new server socket.
+     */
     public void startServer() {
         if (serverSocket != null) {
             stopServer();
@@ -72,6 +85,10 @@ public class Server implements Runnable {
 
     }
 
+
+    /**
+     * Stop the server, and close the server socket.
+     */
     public void stopServer() {
         if (serverSocket != null) {
             try {
@@ -85,6 +102,10 @@ public class Server implements Runnable {
         }
     }
 
+    
+    /**
+     * Start listening for client connections.
+     */
     public void listen() {
         try {
             setDoListen(true);
@@ -95,14 +116,44 @@ public class Server implements Runnable {
             Logger.getLogger(Server.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
-    
+
+
+    /**
+     * Stop listening for new client connections.
+     */
     public void stopListening() {
         setDoListen(false);
     }
 
 
     /**
-     * Accept Clients connecting, and 
+     * Create a normal or proxy connection to this server.
+     */
+    public void connectClient() {
+        clientconnection.ClientConnection myCC;
+
+        if (true == isProxyServer) {
+            // Connect the client to an external server
+            myCC = new clientconnection.ClientConnection(clientSocket, this, proxyAddress, proxyPortNumber);
+            myCC.connectProxyClient();
+        } else {
+            // Otherwise, connect client to this server
+            myCC = new clientconnection.ClientConnection(clientSocket, myClientCommandHandler, this);
+        }
+
+        // Submit the connection to the thread pool
+        threadPool.submit(myCC);
+
+        sendMessageToUI("Client connected:\n\tRemote Socket Address = " +
+                clientSocket.getRemoteSocketAddress() +
+                "\n\tLocal Socket Address = " +
+                clientSocket.getLocalSocketAddress());
+    }
+
+
+    /**
+     * Accept Clients connecting, and creates a ClientConnection for every
+     * client.
      */
     @Override
     public void run() {
@@ -113,7 +164,7 @@ public class Server implements Runnable {
                     connectClient();
                 } catch (SocketException | SocketTimeoutException e) {
                     //check doListen.
-                    if (doListen == false) {
+                    if (!getDoListen()) {
                         myUI.update(e.toString());
                     }
                 } catch (IOException e) {
@@ -130,51 +181,17 @@ public class Server implements Runnable {
 
 
     /**
-     * Create a normal or proxy connection to this server.
+     * Display a message to the server user.
+     * @param theString The message to display.
      */
-    public void connectClient() {
-        clientconnection.ClientConnection myCC;
-
-        if (true == isProxyServer) {
-            // Connect the client to an external server
-            myCC = new clientconnection.ClientConnection(clientSocket, this, proxyAddress, proxyPortNumber);
-            myCC.connectProxyClient();
-
-        } else {
-            // Otherwise, connect client to this server
-            myCC = new clientconnection.ClientConnection(clientSocket, myClientCommandHandler, this);
-        }
-
-        // Create a new Thread and run it
-        Thread myCCthread = new Thread(myCC);
-        myCCthread.start();
-
-        // Notify the server user that a client has connected.
-        sendMessageToUI("Client connected:\n\tRemote Socket Address = " +
-                clientSocket.getRemoteSocketAddress() +
-                "\n\tLocal Socket Address = " +
-                clientSocket.getLocalSocketAddress());
+    public void sendMessageToUI(String theString) {
+        myUI.update(theString);
     }
 
 
-    /**
-     * Set the server's port.
-     * @param portNumber The port to create the server socket on.
-     */
-    public void setPort(int portNumber) {
-        this.portNumber = portNumber;
-    }
-
-
-    /**
-     * Get the port that the server is using.
-     * @return The port number currently being used for the server socket.
-     */
-    public int getPort() {
-        return this.portNumber;
-    }
-
-
+    //--------------------------------------------------------------------------
+    // Getters and Setters
+    //--------------------------------------------------------------------------
     /**
      * Set the address to use and the port number of the server to proxy to.
      * @param address The address to use to connect to the server.
@@ -200,10 +217,36 @@ public class Server implements Runnable {
 
 
     /**
-     * Display a message to the server user.
-     * @param theString The message to display.
+     * Set the server's port.
+     * @param portNumber The port to create the server socket on.
      */
-    public void sendMessageToUI(String theString) {
-        myUI.update(theString);
+    public void setPort(int portNumber) {
+        this.portNumber = portNumber;
+    }
+
+
+    /**
+     * Get the port that the server is using.
+     * @return The port number currently being used for the server socket.
+     */
+    public int getPort() {
+        return this.portNumber;
+    }
+    
+    
+    /**
+     * Set whether the server is listening for connections.
+     * @param doListen true for listening, false for not 
+     */
+    public synchronized void setDoListen(boolean doListen){
+        this.doListen = doListen;
+    }
+    
+    /**
+     * Get whether the server is listening for connections.
+     * @return true if server is listening, false if not
+     */
+    public synchronized boolean getDoListen() {
+        return this.doListen;
     }
 }
